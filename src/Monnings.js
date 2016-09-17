233d7e8fb2bab7ba6877d633e6942aa8ef1b5638
Rasmus Prentow <pseudo@pseudo.local>
2016-09-18 01:35:59 +0200

    Added basic stuff

--- src/Monnings.js
+++ src/Monnings.js
@@ -14,9 +14,27 @@ define(["require", "exports", "phaser", "./Monning"], function (require, exports
             this.game.physics.startSystem(Phaser.Physics.ARCADE);
             //  A simple background for our game
             this.game.add.sprite(0, 0, 'sky');
-            new Monning_1.Monning();
+            this.platforms = this.game.add.group();
+            //  We will enable physics for any object that is created in this group
+            this.platforms.enableBody = true;
+            // Here we create the ground.
+            var ground = this.platforms.create(0, this.game.world.height - 64, 'ground');
+            //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+            ground.scale.setTo(2, 2);
+            //  This stops it from falling away when you jump on it
+            ground.body.immovable = true;
+            //  Now let's create two ledges
+            var ledge = this.platforms.create(790, this.game.world.height - 84, 'ground');
+            ledge.body.immovable = true;
+            ledge = this.platforms.create(-30, this.game.world.height - 84, 'ground');
+            ledge.body.immovable = true;
+            ledge = this.platforms.create(-150, 300, 'ground');
+            ledge.body.immovable = true;
+            this.monning = new Monning_1.Monning(this.game);
         };
         MonningsGame.prototype.update = function () {
+            this.game.physics.arcade.collide(this.monning.sprite, this.platforms);
+            this.monning.update();
         };
         return MonningsGame;
     }());
--- src/Monnings.ts
+++ src/Monnings.ts
@@ -8,6 +8,8 @@ import { Monning} from "./Monning"
 class MonningsGame {
 
     private game: Phaser.Game;
+    private platforms;
+    private monning: Monning;
 
     constructor() {
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: this.preload, create: this.create, update: this.update });
@@ -24,14 +26,43 @@ class MonningsGame {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
         //  A simple background for our game
-        this.game.add.sprite(0, 0, 'sky');
-         new Monning();
+         this.game.add.sprite(0, 0, 'sky');
+         
+
+         this.platforms = this.game.add.group();
+
+    //  We will enable physics for any object that is created in this group
+        this.platforms.enableBody = true;
+
+        // Here we create the ground.
+        var ground = this.platforms.create(0, this.game.world.height - 64, 'ground');
+
+        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+        ground.scale.setTo(2, 2);
+
+        //  This stops it from falling away when you jump on it
+        ground.body.immovable = true;
+
+        //  Now let's create two ledges
+        var ledge = this.platforms.create(790, this.game.world.height - 84, 'ground');
+        ledge.body.immovable = true;
+          ledge = this.platforms.create(-30, this.game.world.height - 84, 'ground');
+        ledge.body.immovable = true;
+        ledge = this.platforms.create(-150, 300, 'ground');
+        ledge.body.immovable = true;
+
+        this.monning = new Monning(this.game);
+
     }
 
     update() {
+        this.game.physics.arcade.collide(this.monning.sprite, this.platforms);
+
+        this.monning.update();
 
     }
  
+
 
 
 }
