c34bf54de658afa1b77d1f2cf2b472642935a028
Rasmus Prentow <pseudo@pseudo.local>
2016-09-18 00:32:40 +0200

    Added requirejs

--- src/Monnings.js
+++ src/Monnings.js
@@ -1,12 +1,24 @@
-/// <reference path="../node_modules/phaser/typescript/phaser.d.ts" />
-var MonningsGame = (function () {
-    function MonningsGame() {
-        this.game = new Phaser.Game();
-    }
-    return MonningsGame;
-}());
-// when the page has finished loading, create our game
-window.onload = function () {
-    var game = new MonningsGame();
-};
-//# sourceMappingURL=Monnings.js.map
+define(["require", "exports", "phaser", "./Monning"], function (require, exports, Phaser, Monning_1) {
+    "use strict";
+    var MonningsGame = (function () {
+        function MonningsGame() {
+            this.game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: this.preload, create: this.create, update: this.update });
+        }
+        MonningsGame.prototype.preload = function () {
+            this.game.load.image('sky', 'assets/sky.png');
+            this.game.load.image('ground', 'assets/platform.png');
+            this.game.load.image('star', 'assets/star.png');
+            this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+        };
+        MonningsGame.prototype.create = function () {
+            this.game.physics.startSystem(Phaser.Physics.ARCADE);
+            //  A simple background for our game
+            this.game.add.sprite(0, 0, 'sky');
+            new Monning_1.Monning();
+        };
+        MonningsGame.prototype.update = function () {
+        };
+        return MonningsGame;
+    }());
+    exports.MonningsGame = MonningsGame;
+});
--- src/Monnings.ts
+++ src/Monnings.ts
@@ -1,7 +1,9 @@
 
 
 /// <reference path="../node_modules/phaser/typescript/phaser.d.ts" />
-import * as Phaser from "phaser"
+import Phaser = require("phaser")
+import { Monning} from "./Monning"
+
 
 class MonningsGame {
 
@@ -12,18 +14,26 @@ class MonningsGame {
     }
 
     preload() {
-
+         this.game.load.image('sky', 'assets/sky.png');
+         this.game.load.image('ground', 'assets/platform.png');
+         this.game.load.image('star', 'assets/star.png');
+         this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     }
 
     create() {
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
+        //  A simple background for our game
+        this.game.add.sprite(0, 0, 'sky');
+         new Monning();
     }
 
     update() {
 
     }
-
+ 
 
 
 }
 
+export {MonningsGame} 
