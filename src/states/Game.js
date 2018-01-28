acb9dc2942dd6b00107c11a12c000445bba7bd6a
Rasmus Prentow <prentow@uber.com>
2018-01-28 20:07:29 +0100

    more cleanup

--- src/states/Game.js
+++ src/states/Game.js
@@ -5,14 +5,11 @@ import ActionManager from '../actions/ActionManager'
 import CountableExecuter from '../util/CountableExecuter'
 
 export default class extends Phaser.State {
-
   monnings: Monning[]
 
   init () {}
 
-  preload () {
-
-  }
+  preload () {}
 
   create () {
     // this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -34,7 +31,6 @@ export default class extends Phaser.State {
     // resizes the game world to match the layer dimensions
     this.backgroundlayer.resizeWorld()
 
-
     const startPos = findObjectsByType('startPosition', this.map, 'objectLayer')[0]
     const actions = new ActionManager(this.game)
     const menu = actions.menu
@@ -42,24 +38,26 @@ export default class extends Phaser.State {
     this.game.add.existing(menu)
     this.monnings = []
 
-    this.executor = new CountableExecuter(() => {
-      const monning = new Monning(this.game, startPos.x, startPos.y, 'dude');
-      monning.events.onInputDown.add((e: any) => {
-        console.log(e)
+    this.executor = new CountableExecuter(
+      () => {
+        const monning = new Monning(this.game, startPos.x, startPos.y, 'dude')
+        monning.events.onInputDown.add((e: any) => {
+          console.log(e)
           actions.activeAction.execute(monning)
+        })
+        this.game.add.existing(monning)
+        if (this.monnings.length === 0) {
+          this.game.camera.follow(monning)
         }
-      )
-      this.game.add.existing(monning)
-      if (this.monnings.length === 0) {
-        this.game.camera.follow(monning)
-      }
-      this.monnings.push(monning)
-    }, 50, 100)
-
+        this.monnings.push(monning)
+      },
+      50,
+      100
+    )
   }
 
   update() {
-    this.monnings.forEach((monning) => {
+    this.monnings.forEach(monning => {
       this.game.physics.arcade.collide(monning, this.blockedLayer)
     })
     this.executor.execute()
@@ -67,14 +65,14 @@ export default class extends Phaser.State {
 
   render () {
     if (__DEV__ && this.monnings.length > 0) {
-     // this.game.debug.spriteInfo(this.monnings.first, 32, 32)
+      // this.game.debug.spriteInfo(this.monnings.first, 32, 32)
     }
   }
 }
 
 function findObjectsByType (type: String, map: Phaser.Tilemap, layer: String): any {
   const result = []
-  map.objects[layer].forEach((element) => {
+  map.objects[layer].forEach(element => {
     if (element.properties && element.properties.type === type) {
       element.y -= map.tileHeight
       result.push(element)
